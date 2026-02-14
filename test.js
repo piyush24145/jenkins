10e4633d67a92d73de4f5997e7e9a91b676e4a56
piyush kumar <24145@iiitu.ac.in>
2026-02-14 15:44:21 +0530

    check ngrok1

--- test.js
+++ test.js
@@ -1,19 +1,10 @@
 console.log("Running Tests...");
 
 if (1 + 1 === 2) {
-  console.log("Test Passed ✅");
+  console.log("Test Passed hello baby");
   process.exit(0);
 } else {
   console.log("Test Failed ❌");
   process.exit(1);
 }
 
-console.log("Running testb 2");
-
-if(3+4==7){
-  console.log("test passed second time ");
-    process.exit(0);
-}else{
-   console.log("Test Failed ❌");
-  process.exit(1);
-}
